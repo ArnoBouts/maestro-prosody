d736f77e198faa649fba17d159da885a95765fd4
Stoub <eve@no-cloud.fr>
2018-05-11 08:02:54 +0200

    fix sed issue

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,10 @@ RUN apt-get update \
 	&& apt-get remove -y mercurial \
 	&& rm -rf /var/lib/apt/lists/*
 
-ADD ./config/prosody.cfg.lua /etc/prosody
-RUN chown prosody /etc/prosody/prosody.cfg.lua
+ADD ./config/prosody.cfg.lua /var/lib/prosody
+RUN chown -R prosody /var/lib/prosody
+
+RUN ln -sf /var/lib/prosody/prosody.cfg.lua /etc/prosody/prosody.cfg.lua
 
 COPY ./entrypoint.sh /entrypoint.sh
 RUN chmod 755 /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+echo "SED"
 # if first start, init conf
-sed -i "s/\${DOMAIN}/${DOMAIN}/g" /etc/prosody/prosody.cfg.lua
-sed -i "s/\${LDAP_ADMIN_DN}/${LDAP_ADMIN_DN}/g" /etc/prosody/prosody.cfg.lua
-sed -i "s/\${LDAP_ADMIN_PASSWORD}/${LDAP_ADMIN_PASSWORD}/g" /etc/prosody/prosody.cfg.lua
+sed -i "s/\${DOMAIN}/${DOMAIN}/g" /var/lib/prosody/prosody.cfg.lua
+sed -i "s/\${LDAP_ADMIN_DN}/${LDAP_ADMIN_DN}/g" /var/lib/prosody/prosody.cfg.lua
+sed -i "s/\${LDAP_ADMIN_PASSWORD}/${LDAP_ADMIN_PASSWORD}/g" /var/lib/prosody/prosody.cfg.lua
 
+echo "Start"
 if [[ "$1" != "prosody" ]]; then
     exec prosodyctl $*
     exit 0;
